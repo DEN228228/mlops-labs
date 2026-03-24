e201859b09399dfe0b82e87ed2f1a77d4eaf6259
Denys Svintsylo <denys.svintsylo@honeycombsoft.com>
2026-03-24 17:31:18 +0200

    format

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,9 +4,7 @@ import pandas as pd
 
 def test_data_schema_basic():
 
-    data_path = os.getenv(
-        "DATA_PATH", "data/raw/cleaned_house_rent_data_cropped.csv"
-    )
+    data_path = os.getenv("DATA_PATH", "data/raw/cleaned_house_rent_data_cropped.csv")
 
     assert os.path.exists(data_path), f"Дані не знайдено за шляхом: {data_path}"
 
